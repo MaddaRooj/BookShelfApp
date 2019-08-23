5103335d8f69bce788d3c85e88f98c1d1594a2a1
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-08-23 13:53:01 -0500

    first commit

---
+++ BookShelfApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BookShelfApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShelfApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Book> Book { get; set; }
+    }
+}
